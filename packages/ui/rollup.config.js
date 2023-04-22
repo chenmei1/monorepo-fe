25bb122ab11dfd1cc9f5654c69387b9dea6f5b43
ltProcess <lt41156004@163.com>
2023-04-22 14:54:17 +0800

    fix: react file

--- packages/ui/rollup.config.js
+++ packages/ui/rollup.config.js
@@ -1,66 +1,13 @@
-import { defineConfig } from 'rollup';
-import commonjs from '@rollup/plugin-commonjs';
+import { defineConfig } from 'rollup'
+import commonjs from '@rollup/plugin-commonjs'
 import postcss from 'rollup-plugin-postcss'
-import { nodeResolve as resolve } from '@rollup/plugin-node-resolve';
-import typescript from 'rollup-plugin-typescript2';
-import vue from 'rollup-plugin-vue';
-import { babel } from '@rollup/plugin-babel';
-import { name } from './package.json';
+import { nodeResolve as resolve } from '@rollup/plugin-node-resolve'
+import typescript from 'rollup-plugin-typescript2'
+import vue from 'rollup-plugin-vue'
+import { babel } from '@rollup/plugin-babel'
+import { name } from './package.json'
 
 export default defineConfig([
-  {
-    input: 'react/index.tsx',
-    external: ['react', 'react-dom'],
-    plugins: [
-      typescript(),
-      resolve({
-        extensions: ['.tsx', '.ts', '.js']
-      }),
-      postcss({}),
-      babel({
-        babelrc: false,
-        exclude: '**/node_modules/**',
-        presets: ['@babel/preset-react', '@babel/preset-env'],
-        plugins: [
-          '@babel/plugin-proposal-object-rest-spread',
-          '@babel/plugin-syntax-object-rest-spread',
-          '@babel/plugin-transform-react-jsx',
-          [
-            '@babel/plugin-transform-runtime',
-            {
-              absoluteRuntime: false,
-              corejs: false,
-              helpers: false,
-              regenerator: false,
-              useESModules: false,
-            },
-          ],
-        ],
-      }),
-      commonjs(),
-    ],
-    output: [
-      {
-        name,
-        file: './dist/react/index.js',
-        format: 'umd',
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM'
-        }
-      },
-      {
-        name,
-        file: './es/react/index.js',
-        format: 'es',
-      },
-      {
-        name,
-        file: './lib/react/index.cjs',
-        format: 'commonjs',
-      }
-    ]
-  },
   {
     input: 'vue/index.ts',
     external: ['vue'],
@@ -68,7 +15,7 @@ export default defineConfig([
       vue(),
       typescript(),
       resolve({
-        extensions: ['.ts', '.js', '.vue'],
+        extensions: ['.ts', '.js', '.vue']
       }),
       postcss({}),
       commonjs(),
@@ -84,13 +31,22 @@ export default defineConfig([
               corejs: false,
               helpers: false,
               regenerator: true,
-              useESModules: false,
-            },
-          ],
+              useESModules: false
+            }
+          ]
         ],
         babelrc: false,
-        extensions: ['.js', '.ts', '.tsx', '.jsx', '.es6', '.es', '.mjs', '.vue'],
-      }),
+        extensions: [
+          '.js',
+          '.ts',
+          '.tsx',
+          '.jsx',
+          '.es6',
+          '.es',
+          '.mjs',
+          '.vue'
+        ]
+      })
     ],
     output: [
       {
@@ -99,18 +55,18 @@ export default defineConfig([
         format: 'umd',
         exports: 'named',
         globals: {
-          vue: 'Vue',
+          vue: 'Vue'
         }
       },
       {
         name,
         file: './es/vue/index.js',
-        format: 'es',
+        format: 'es'
       },
       {
         name,
         file: './lib/vue/index.cjs',
-        format: 'commonjs',
+        format: 'commonjs'
       }
     ]
   }
